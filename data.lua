020ff19a725c3ca03d5be42fba3a128691bc3fe1
adam wong <adamwong246@gmail.com>
2021-02-18 13:50:08 -0800

    add waterAsResource and remove free spidertron energy

--- data.lua
+++ data.lua
@@ -1,9 +1,16 @@
 --data.lua
 
 local pfr = table.deepcopy(data.raw["generator-equipment"]["fusion-reactor-equipment"])
+
 pfr.burner = {
   type = "burner",
   fuel_inventory_size = 10
 }
 
-data:extend{pfr}
+local spider = table.deepcopy(data.raw["spider-vehicle"]["spidertron"])
+spider.energy_source = {
+  type = "burner",
+  fuel_inventory_size = 10
+}
+
+data:extend{pfr, spider}
